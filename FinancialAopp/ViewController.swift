9ef069157f0115a5fa3474fe85c67a00e9d80320
Dinesh Sharma <dineshsharma@Sudhirs-Mac-mini-2.local>
2023-01-23 20:29:25 +0530

    Initial commit

---
+++ FinancialAopp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinancialAopp
+//
+//  Created by Dinesh Sharma on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
